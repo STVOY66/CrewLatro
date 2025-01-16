b6b29ff18d1e39bbc74c6e5b6348c43c376dee1a
Ethan Pauly <ethan.pauly@gmail.com>
2025-01-16 17:35:20 -0500

    Test-ready joker

--- Jokers.lua
+++ Jokers.lua
@@ -1,1 +1,29 @@
-local farenheight = {}
+SMODS.Atlas {
+    key = "CrewLatroAtlas",
+    path = "CrewLatroAtlas.png",
+    px = 71, py = 95
+}
+
+SMODS.Joker {
+    key = 'fahrenheit',
+    loc_txt = {
+        name = 'Burn the Books',
+        text = {"If first discard is a {C:attention}4{}, {C:attention}5{}, or an {C:attention}Ace{},", "destroy card and gain {C:money}$#1#{}."}
+    },
+
+    config = { extra = { payout = 3 }},
+    loc_vars = function(self, info_queue, card)
+        return {vars = {card.ability.extra.payout}}
+    end,
+
+    rarity = 1,
+    atlas = "CrewLatroAtlas",
+    pos = { x = 0, y = 0 },
+    cost = 6,
+
+    calculate = function(self, card, context)
+        if context.discard and not context.other_card.debuff and (context.other_card:get_id() == 4 or context.other_card:get_id() == 5 or context.other_card:get_id() == 14) then
+            return { dollars = card.ability.extra.payout }
+        end
+    end
+}
