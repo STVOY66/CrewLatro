5f818b5f1ac36b1d68b4b36afe55e91bb4b2d10d
Ethan Pauly <ethan.pauly@gmail.com>
2025-01-28 20:28:22 -0500

    Added pickle sounds

--- sounds.lua
+++ sounds.lua
@@ -12,3 +12,13 @@ SMODS.Sound {
     key = 'crew_gamble_winner',
     path = 'gamble_payout.ogg',
 }
+
+SMODS.Sound {
+    key = 'crew_picklesound',
+    path = 'pickle_.ogg',
+}
+
+SMODS.Sound {
+    key = 'crew_pickle_no_good',
+    path = 'pickle_no_good.ogg',
+}
